468b45384d395fedde23479ddbff14c6773e410e
Hugo Tausch <hugoskummerkasten@gmail.com>
2023-04-01 15:06:38 +0200

    updated detail data

--- lib/pages/detail_elementes/event_detail_data.dart
+++ lib/pages/detail_elementes/event_detail_data.dart
@@ -1,6 +1,11 @@
 import 'package:cjvm_app/model/event_entitiy.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:intl/intl.dart';
+import '../../utils/color_utils.dart' as color_utils;
 
 class EventDetailData extends StatelessWidget {
   final EventEntity event;
@@ -8,6 +13,61 @@ class EventDetailData extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    bool venueSet = event.venue == "" ? false : true;
+    return Column(
+      children: [
+        if (venueSet)
+          Row(
+            children: [
+              const Icon(
+                CupertinoIcons.map_pin_ellipse,
+                size: 35,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    event.venue,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                  Text(
+                    event.address,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                ],
+              )
+            ],
+          ),
+        Row(
+          children: [
+            Icon(
+              PlatformIcons(context).time,
+              size: 35,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  event.allDay
+                      ? "${DateFormat.MEd('de').format(event.startDate)} bis ${DateFormat.MEd('de').format(event.endDate)}"
+                      : DateFormat.MMMMEEEEd('de').format(event.startDate),
+                ),
+                if (!event.allDay)
+                  Text(
+                    style: Theme.of(context).textTheme.bodyMedium,
+                    "${DateFormat.Hm('de').format(event.startDate)}Uhr bis ${DateFormat.Hm('de').format(event.endDate)}Uhr",
+                  ),
+              ],
+            )
+          ],
+        ),
+        Container(
+          height: 3.0,
+          width: MediaQuery.of(context).size.width,
+          color: color_utils.commonThemeData.primaryColor,
+        ),
+      ],
+    );
   }
 }
--- lib/pages/detail_elementes/post_detail_data.dart
+++ lib/pages/detail_elementes/post_detail_data.dart
@@ -12,44 +12,35 @@ class PostDetailData extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Padding(
-          padding: const EdgeInsets.all(8),
-          child: Row(
-            children: [
-              Icon(
-                PlatformIcons(context).time,
-                size: 35,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8),
-                child: Column(
+        Row(
+          children: [
+            Icon(
+              PlatformIcons(context).time,
+              size: 35,
+            ),
+            Column(
+              children: [
+                Row(
                   children: [
-                    Row(
-                      children: [
-                        Text(
-                          "Aktualisiert: ",
-                          style: Theme.of(context).textTheme.bodyMedium,
-                        ),
-                        Text(
-                          DateFormat.yMMMd('de')
-                              .format(DateTime.parse(post.modifiedGmt)),
-                          style: Theme.of(context).textTheme.bodyMedium,
-                        ),
-                      ],
+                    Text(
+                      "Aktualisiert: ",
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                    Text(
+                      DateFormat.yMMMd('de')
+                          .format(DateTime.parse(post.modifiedGmt)),
+                      style: Theme.of(context).textTheme.bodyMedium,
                     ),
                   ],
                 ),
-              ),
-            ],
-          ),
+              ],
+            ),
+          ],
         ),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 8),
-          child: Container(
-            height: 3.0,
-            width: MediaQuery.of(context).size.width,
-            color: color_utils.commonThemeData.primaryColor,
-          ),
+        Container(
+          height: 3.0,
+          width: MediaQuery.of(context).size.width,
+          color: color_utils.commonThemeData.primaryColor,
         ),
       ],
     );
